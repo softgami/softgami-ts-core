b5876fae088cc96150f1e4eb13d3b335145d832c
Lucas Mendes da luz <lucas@MacBook-Pro-de-Lucas.local>
2018-08-14 22:12:25 -0300

    feat(exports): exporting modules typescript, ng6 and nodejs.

--- projects/softgami-ts-core/src/public_api.ts
+++ projects/softgami-ts-core/src/public_api.ts
@@ -5,3 +5,7 @@
 export * from './lib/softgami-ts-core.service';
 export * from './lib/softgami-ts-core.component';
 export * from './lib/softgami-ts-core.module';
+
+export * from './lib/typescript/index';
+export * from './lib/ng6/index';
+export * from './lib/nodejs/index';

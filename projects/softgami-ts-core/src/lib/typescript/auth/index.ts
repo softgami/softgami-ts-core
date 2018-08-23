568ba3c68a564e9195f5988641a43916a8b34d8c
Luz <lslz@gft.com>
2018-08-23 15:52:22 -0300

    feat(credentials): exposing credentials models to public api.

--- projects/softgami-ts-core/src/lib/typescript/auth/index.ts
+++ projects/softgami-ts-core/src/lib/typescript/auth/index.ts
@@ -1,2 +1,4 @@
 export { BasicPassword } from './password/basic-password.interface';
+export { BasicCredentials } from './credentials/basic-credentials.interface';
+export { Credentials } from './credentials/credentials.interface';
 export { Password } from './password/password.interface';

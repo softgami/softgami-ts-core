b6e801a4935d4394d849330439de8ecf30221cad
Luz <lslz@gft.com>
2018-09-06 13:49:30 -0300

    refactor(permissions): refactoring permissions and actions.

--- projects/softgami-ts-core/src/lib/core/permissions/action/basic-action.interface.ts
+++ projects/softgami-ts-core/src/lib/core/permissions/action/basic-action.interface.ts
@@ -1,6 +1,7 @@
 export interface BasicAction {
     name: string;
     alias: string;
+    value: number;
     description?: string;
     createdAt?: Date;
     lastUpdate?: Date;
--- projects/softgami-ts-core/src/lib/core/permissions/permission/basic-permission.ts
+++ projects/softgami-ts-core/src/lib/core/permissions/permission/basic-permission.ts
@@ -1,9 +1,7 @@
-import { Action } from '../action/action.interface';
 import { Role } from '../role/role.interface';
 import { Subject } from '../subject/subject.interface';
 
 export interface BasicPermission {
-    action: Action;
     subject: Subject;
     role: Role;
     value: number;

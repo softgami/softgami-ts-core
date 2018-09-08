da6f5f7566336c0e01ad949155f8f5bbac44ca46
Lucas Mendes da luz <lucas@MacBook-Pro-de-Lucas.local>
2018-09-08 16:05:00 -0300

    fix(gamiBooks): fixing exporting models.

--- projects/softgami-ts-core/src/lib/index.ts
+++ projects/softgami-ts-core/src/lib/index.ts
@@ -3,6 +3,7 @@ export * from './auth/index';
 export * from './core/index';
 export * from './domain/index';
 export * from './email/index';
+export * from './gami-books/index';
 export * from './location/index';
 export * from './phone/index';
 export * from './shared/index';

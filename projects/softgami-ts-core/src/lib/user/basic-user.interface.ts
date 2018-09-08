1a842ccb76e15350f6560409035912863243d0ac
Lucas Mendes da luz <lucas@MacBook-Pro-de-Lucas.local>
2018-09-08 15:12:02 -0300

    refactor(users): basic users now extends basic person.

--- projects/softgami-ts-core/src/lib/user/basic-user.interface.ts
+++ projects/softgami-ts-core/src/lib/user/basic-user.interface.ts
@@ -1,35 +1,26 @@
 import { Address } from '../location/address/address.interface';
 import { AppInstance } from '../app/app-instance/app-instance.interface';
-import { Country } from '../location/country/country.interface';
+import { BasicPerson } from '../shared/person/basic-person.interface';
 import { Credentials } from '../auth/credentials/credentials.interface';
 import { Email } from '../email/email.interface';
-import { Gender } from '../domain/gender/gender.enum';
 import { Income } from '../domain/income/income.interface';
 import { Language } from '../core/i18n/language/language.interface';
 import { MaritalStatus } from '../domain/marital-status/marital-status.interface';
 import { Occupation } from '../domain/occupation/occupation.interface';
 import { Phone } from '../phone/phone.interface';
 
-export interface BasicUser {
-    name: string;
+export interface BasicUser extends BasicPerson {
     emails: Array<Email>;
     credentials?: Credentials;
     isActive?: boolean;
     isIndividual?: boolean;
     language?: Language;
     timezone?: string;
-    surname?: string;
-    birthDate?: Date;
-    picture?: string;
     taxNumber?: string;
-    gender?: Gender;
     phones?: Array<Phone>;
     maritalStatus?: MaritalStatus;
     occupation?: Occupation;
     income?: Income;
-    nationality?: Country;
     addresses?: Array<Address>;
     appInstances?: Array<AppInstance>;
-    createdAt?: Date;
-    lastUpdate?: Date;
 }

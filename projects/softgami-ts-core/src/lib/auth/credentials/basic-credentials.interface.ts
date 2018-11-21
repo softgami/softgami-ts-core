298f81411aaf8a274adba653b171f7ef7c464585
Luz <lslz@gft.com>
2018-11-21 15:44:23 -0200

    refactor(credentials): optional fields.

--- projects/softgami-ts-core/src/lib/auth/credentials/basic-credentials.interface.ts
+++ projects/softgami-ts-core/src/lib/auth/credentials/basic-credentials.interface.ts
@@ -2,7 +2,7 @@ import { Password } from '../password/password.interface';
 
 export interface BasicCredentials {
     current: Password;
-    new: Password;
-    confirm: Password;
+    new?: Password;
+    confirm?: Password;
     old?: Array<Password>;
 }

5c20f072e2337464cb31a39b484e9f5931c197a6
Luz <lslz@gft.com>
2019-02-01 14:38:08 -0200

    feat(date-time): date time periods enum.

---
+++ projects/softgami-ts-core/src/lib/domain/date-time/date-time-periods.enum.ts
@@ -0,0 +1,13 @@
+export enum DateTimePeriods {
+    DAILY = 'daily',
+    NIGHTLY = 'nightly',
+    WEEKLY = 'weekly',
+    BIWEEKLY = 'biweekly',
+    HOURLY = 'hourly',
+    YEARLY = 'yearly',
+    BIYEARLY = 'biyearly',
+    TRIENNIAL = 'triennial',
+    MONTHLY = 'monthly',
+    BIMONTHLY = 'bimonthly',
+    SEMESTRAL = 'semestral',
+}
--- projects/softgami-ts-core/src/lib/domain/index.ts
+++ projects/softgami-ts-core/src/lib/domain/index.ts
@@ -1,6 +1,7 @@
 export { BasicIncome } from './income/basic-income.interface';
 export { BasicMaritalStatus} from './marital-status/basic-marital-status.interface';
 export { BasicOccupation } from './occupation/basic-occupation.interface';
+export { DateTimePeriods } from './date-time/date-time-periods.enum';
 export { Gender } from './gender/gender.enum';
 export { Income } from './income/income.interface';
 export { MaritalStatus } from './marital-status/marital-status.interface';

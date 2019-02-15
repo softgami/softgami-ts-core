36fbf78af174da28f980a972f42c6d250956120a
Luz <lslz@gft.com>
2019-02-15 17:24:32 -0200

    feat(files): file models.

---
+++ projects/softgami-ts-core/src/lib/core/file/basic-file.interface.ts
@@ -0,0 +1,23 @@
+import { AppInstance } from '../../app/app-instance/app-instance.interface';
+import { File } from './file.interface';
+import { User } from '../../user/user.interface';
+
+export interface BasicFile {
+    name: string;
+    isFile: boolean;
+    isDirectory: boolean;
+    path?: string;
+    isPublic?: boolean;
+    mimetype?: string;
+    size?: number;
+    parent?: File;
+    ancestors?: File[];
+    creator?: User;
+    appInstance?: AppInstance;
+    downloads?: {
+        user: User,
+        downloadedAt: Date,
+    }[];
+    createdAt?: Date;
+    lastUpdate?: Date;
+}
---
+++ projects/softgami-ts-core/src/lib/core/file/file.interface.ts
@@ -0,0 +1,5 @@
+import { BasicFile } from './basic-file.interface';
+
+export interface File extends BasicFile {
+    _id: string;
+}

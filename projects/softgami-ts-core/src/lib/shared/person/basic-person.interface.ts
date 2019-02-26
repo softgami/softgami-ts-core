6ecf743c44214a90c0acb4e1ab026734491e80f0
Luz <lslz@gft.com>
2019-02-26 17:16:27 -0300

    refactor(persons): basic persons extends Thing.

--- projects/softgami-ts-core/src/lib/shared/person/basic-person.interface.ts
+++ projects/softgami-ts-core/src/lib/shared/person/basic-person.interface.ts
@@ -1,20 +1,15 @@
 import { Country } from '../../location/country/country.interface';
 import { Gender } from '../../domain/gender/gender.enum';
 import { QuantitativeValue } from '../../core/value/quantitative-value.interface';
+import { Thing } from '../thing/thing.interface';
 
-export interface BasicPerson {
-    name: string;
-    description?: string;
+export interface BasicPerson extends Thing {
     gender?: Gender;
     birthDate?: Date;
     deathDate?: Date;
     birthPlace?: Country;
     nationality?: Country;
-    url?: string;
-    image?: string;
     height?: QuantitativeValue;
     weight?: QuantitativeValue;
     jobTitle?: string;
-    createdAt?: Date;
-    lastUpdate?: Date;
 }

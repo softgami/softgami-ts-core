e982c65ff1813abf544a0521c636355451547576
Luz <lslz@gft.com>
2019-02-27 11:04:09 -0300

    chorus(1.6.0): v1.6.0

--- projects/softgami-ts-core/src/lib/shared/index.ts
+++ projects/softgami-ts-core/src/lib/shared/index.ts
@@ -1,5 +1,7 @@
 export { BasicPerson } from './person/basic-person.interface';
+export { Organization } from './organization/organization.interface';
 export { Person } from './person/person.interface';
 export { Phone } from './phone/phone.interface';
 export { PhoneType } from './phone/phone-type.enum';
 export { SharedModule } from './shared.module';
+export { Thing } from './thing/thing.interface';

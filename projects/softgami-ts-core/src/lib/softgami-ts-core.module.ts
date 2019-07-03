18122f8ccb2e555054747187021202847a6b7730
Luz <lslz@gft.com>
2019-07-03 17:50:29 -0300

    feat(angular): removing angular dependencies from lib softgami-ts-core.

--- projects/softgami-ts-core/src/lib/softgami-ts-core.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { SoftgamiTsCoreComponent } from './softgami-ts-core.component';
-
-@NgModule({
-    imports: [],
-    declarations: [SoftgamiTsCoreComponent],
-    exports: [SoftgamiTsCoreComponent]
-})
-export class SoftgamiTsCoreModule { }
--- projects/softgami-ts-core/src/public_api.ts
+++ projects/softgami-ts-core/src/public_api.ts
@@ -2,8 +2,4 @@
  * Public API Surface of softgami-ts-core
  */
 
-export * from './lib/softgami-ts-core.service';
-export * from './lib/softgami-ts-core.component';
-export * from './lib/softgami-ts-core.module';
-
 export * from './lib/index';

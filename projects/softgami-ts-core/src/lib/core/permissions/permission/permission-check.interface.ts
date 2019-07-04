f50e8b528a640321420c141532cfbb5989128b74
Luz <lslz@gft.com>
2019-07-04 17:50:05 -0300

    feat(permissions): adding role alias to permissions check.

--- projects/softgami-ts-core/src/lib/core/permissions/permission/permission-check.interface.ts
+++ projects/softgami-ts-core/src/lib/core/permissions/permission/permission-check.interface.ts
@@ -1,9 +1,11 @@
 import { Actions } from '../action/actions.enum';
 import { SubjectAlias } from '../subject/subject-alias.enum';
+import { RoleAlias } from '../role/role-alias.enum';
 
 export interface PermissionCheck {
     subject: SubjectAlias;
     action: Actions;
+    role?: RoleAlias;
     shouldValidateBodyAppInstance?: boolean;
     shouldValidateQueryAppInstance?: boolean;
 }

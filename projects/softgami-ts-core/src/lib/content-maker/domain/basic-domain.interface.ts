82bcec37a62cc85863cca4fc24d8044f27a51de1
Lucas Mendes da luz <lucas@MacBook-Pro-de-Lucas.local>
2019-10-02 20:27:15 -0300

    feat(domains): additional properties.

--- projects/softgami-ts-core/src/lib/content-maker/domain/basic-domain.interface.ts
+++ projects/softgami-ts-core/src/lib/content-maker/domain/basic-domain.interface.ts
@@ -1,8 +1,12 @@
+import { AppInstance } from '../../core/app/app-instance/app-instance.interface';
 import { DomainType } from './domain-type.enum';
 import { Thing } from '../../core/shared/thing/thing.interface';
+import { User } from '../../core/user/user.interface';
 
 export interface BasicDomain<T extends DomainType> extends Thing {
     type: T;
     value: string;
     additionalMetadata?: string;
+    creator?: User;
+    appInstance?: AppInstance;
 }

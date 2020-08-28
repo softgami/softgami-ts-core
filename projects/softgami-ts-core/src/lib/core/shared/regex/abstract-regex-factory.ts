cacf79ea8a71c4684a180d8a373eb30fa9bc47ee
Softgami <softgami@gmail.com>
2020-08-28 18:09:21 -0300

    feat(shared): regex factories by language added.

--- projects/softgami-ts-core/src/lib/core/shared/regex/abstract-regex-factory.ts
+++ projects/softgami-ts-core/src/lib/core/shared/regex/abstract-regex-factory.ts
@@ -1,6 +1,21 @@
 import { Country } from '../../../content-maker/location/country/country.model';
+import { Language } from '../../../content-maker/i18n/language/language.model';
 
 export abstract class AbstractRegexFactory {
-    abstract getRegexFromCountry(country: Country): RegExp;
-    abstract getRegexFromCountryCode(code: string): RegExp;
+
+    abstract getRegexByLocale(locale: string): RegExp;
+    abstract getRegexByCountryCode(code: string): RegExp;
+
+    getRegexByCountry(country: Country): RegExp {
+
+        return this.getRegexByCountryCode(country ? country.code : null);
+
+    }
+
+    getRegexByLanguage(language: Language): RegExp {
+
+        return this.getRegexByLocale(language ? language.code : null);
+
+    }
+
 }
--- projects/softgami-ts-core/src/lib/core/shared/regex/phone/phone-regex-factory.service.ts
+++ projects/softgami-ts-core/src/lib/core/shared/regex/phone/phone-regex-factory.service.ts
@@ -1,21 +1,27 @@
 import { AbstractRegexFactory } from '../abstract-regex-factory';
-import { Country } from '../../../../content-maker/location/country/country.model';
 import { VALID_PHONE_BR_REGEX } from './valid-phone-br.regex';
 
 export class PhoneRegexFactoryService extends AbstractRegexFactory {
 
-    getRegexFromCountry(country: Country): RegExp {
-
-        return this.getRegexFromCountryCode(country ? country.code : null);
-
-    }
-
-    getRegexFromCountryCode(code: string): RegExp {
+    getRegexByCountryCode(code: string): RegExp {
 
         if (!code) code = 'br';
 
         switch (code.toLowerCase()) {
             case 'br':
+                return VALID_PHONE_BR_REGEX;
+            default:
+                return VALID_PHONE_BR_REGEX;
+        }
+
+    }
+
+    getRegexByLocale(locale: string): RegExp {
+
+        if (!locale) locale = 'pt-br';
+
+        switch (locale.toLowerCase()) {
+            case 'pt-br':
                 return VALID_PHONE_BR_REGEX;
             default:
                 return VALID_PHONE_BR_REGEX;

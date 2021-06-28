bc3928c8e1444a13e598e480c690f0cabe3aef70
Softgami <softgami@gmail.com>
2021-06-28 18:16:15 -0300

    refactor(translations): renaming folder.

--- projects/softgami-ts-core/src/lib/content-maker/i18n/index.ts
+++ projects/softgami-ts-core/src/lib/content-maker/i18n/index.ts
@@ -1,3 +1,3 @@
 export { Language } from './language/language.model';
-export { Translation } from './translations/translation.model';
-export { TranslationType } from './translations/translation-types.enum';
+export { Translation } from './translation/translation.model';
+export { TranslationType } from './translation/translation-types.enum';
